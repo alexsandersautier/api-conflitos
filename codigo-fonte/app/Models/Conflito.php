f42160e76647a66c4e99c0a3068f3e9446fbf4e3
Luiz Leão <luizleao@gmail.com>
2025-06-26 11:08:09 -0300

    Ajustes
    Ajustes conflito Controller
    TipoEstrategiaTableSeeder

--- codigo-fonte/app/Models/Conflito.php
+++ codigo-fonte/app/Models/Conflito.php
@@ -26,32 +26,33 @@ class Conflito extends Model
                            'flagOcorrenciaAssassinato',
                            'flagOcorrenciaFeridos',
                            'flagMembroProgramaProtecao'];
-
-    public function terras_indigenas() {
-        return $this->belongsToMany(TerraIndigena::class, 'terra_indigena_conflito', 'idConflito', 'idTerraIndigena');
-    }
-
-    public function povos() {
-        return $this->belongsToMany(Povo::class, 'povo_conflito', 'idConflito', 'idPovo');
-    }
-
+    
     public function assuntos(){
         return $this->belongsToMany(Assunto::class, 'assunto_conflito', 'idConflito', 'idAssunto');
     }
 
-    public function tiposconflito(){
-        return $this->belongsToMany(TipoConflito::class, 'conflito_tipo_conflito', 'idConflito', 'idTipoConflito');
-    }
-
-    public function impactosambientais(){
+    public function impactos_ambientais(){
         return $this->belongsToMany(ImpactoAmbiental::class, 'impacto_ambiental_conflito', 'idConflito', 'idImpactoAmbiental');
     }
-
-    public function impactossaude(){
+    
+    public function impactos_saude(){
         return $this->belongsToMany(ImpactoSaude::class, 'impacto_saude_conflito', 'idConflito', 'idImpactoSaude');
     }
-
-    public function impactosSocioEconomicos(){
+    
+    public function impactos_socio_economicos(){
         return $this->belongsToMany(ImpactoSocioEconomico::class, 'impacto_socio_economico_conflito', 'idConflito', 'idImpactoSocioEconomico');
     }
+    
+    public function povos() {
+        return $this->belongsToMany(Povo::class, 'povo_conflito', 'idConflito', 'idPovo');
+    }
+    
+    public function terras_indigenas() {
+        return $this->belongsToMany(TerraIndigena::class, 'terra_indigena_conflito', 'idConflito', 'idTerraIndigena');
+    }
+    
+    public function tipos_conflito(){
+        return $this->belongsToMany(TipoConflito::class, 'conflito_tipo_conflito', 'idConflito', 'idTipoConflito');
+    }
+    
 }
