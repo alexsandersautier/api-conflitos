12cf883054d8598fa0300915d97c8399a68ee611
Luiz Leão <luizleao@gmail.com>
2025-07-02 21:48:15 -0300

    Ajustes Deploy Jenkins
    Docker e outros arquivos

--- codigo-fonte/app/Http/Controllers/Controller.php
+++ codigo-fonte/app/Http/Controllers/Controller.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 /**
  * @OA\Info(
  *      version="1.0.0",
- *      title="MPI API Documentação",
- *      description="Documentação da API do sistema MPI",
+ *      title="paineldeconflitos-api - Documentação",
+ *      description="Catálogo de serviços de paineldeconflitos-api",
  *      @OA\Contact(
  *          email="luizleao@gmail.com"
  *      )
